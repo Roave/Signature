d5af64c784f3b103d16898278c1aa32e526260b2
Jefersson Nathan <admin@phpse.net>
2016-11-16 17:22:22 -0300

    Added test class doc block

--- test/unit/src/SignerTest.php
+++ test/unit/src/SignerTest.php
@@ -10,6 +10,15 @@ use Signature\Hasher\Md5Hasher;
 use Zend\Code\Generator\ClassGenerator;
 use Zend\Code\Generator\PropertyGenerator;
 
+/**
+ * Tests for {@see Signature\Signer}.
+ *
+ * @group   Unitary
+ * @author  Jefersson Nathan <malukenho@phpse.net>
+ * @license MIT
+ *
+ * @covers \Signature\Signer
+ */
 final class SignerTest extends \PHPUnit_Framework_TestCase
 {
     public function testItShouldSignAClass()
