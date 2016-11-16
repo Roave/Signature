ce4d31b5dcd79e8bee6a1685636220ec1015a127
Jefersson Nathan <admin@phpse.net>
2016-11-16 15:13:41 -0300

    Turn signer class final

--- src/Signer.php
+++ src/Signer.php
@@ -9,7 +9,7 @@ use Signature\Hasher\HasherInterface;
 use Zend\Code\Generator\ClassGenerator;
 use Zend\Code\Generator\PropertyGenerator;
 
-class Signer implements SignerInterface
+final class Signer implements SignerInterface
 {
     /**
      * @var HasherInterface
