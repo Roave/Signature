391fd6c7e289ac1a6be284bb0d298d94e99e4234
Jefersson Nathan <admin@phpse.net>
2016-11-16 17:27:02 -0300

    Add test to signature checker

---
+++ test/unit/src/CheckerTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SignatureTest;
+
+use Signature\Checker;
+use Signature\Encoder\Base64Encoder;
+use Signature\Encoder\EncoderInterface;
+use Signature\Hasher\HasherInterface;
+use Signature\Hasher\Md5Hasher;
+use SignatureTestFixture\ClassWithValidSignerProperty;
+
+/**
+ * Tests for {@see Signature\Checker}.
+ *
+ * @group   Unitary
+ * @author  Jefersson Nathan <malukenho@phpse.net>
+ * @license MIT
+ *
+ * @covers \Signature\Checker
+ */
+final class CheckerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCheckInvalidSignature()
+    {
+        $checker = new Checker(new Base64Encoder(), new Md5Hasher());
+
+        $this->expectException(\RuntimeException::class);
+
+        $checker->check(new \ReflectionClass(\stdClass::class), []);
+    }
+
+    public function testCheck()
+    {
+        $encoder = $this->createMock(EncoderInterface::class);
+        $hasher  = $this->createMock(HasherInterface::class);
+
+        $encoder->expects(self::exactly(1))->method('encode')->with([])->willReturn('YTowOnt9');
+        $hasher->expects(self::exactly(1))->method('hash')->with([])->willReturn('40cd750bba9870f18aada2478b24840a');
+
+        $checker = new Checker($encoder, $hasher);
+
+        $checker->check(new \ReflectionClass(ClassWithValidSignerProperty::class), []);
+    }
+}
